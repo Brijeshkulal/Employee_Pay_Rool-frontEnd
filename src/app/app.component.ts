b3af0b2178abdd05e284512fb21127451835f637
“Brijesh” <brijeshkulal5@gmail.com>
2022-01-01 21:28:21 +0530

    Added Routing and services for REST call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from './service/employee.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Employee_PayRoll';
+  title = 'Employee_Pay_Roll';
+ constructor(private employee: EmployeeService){}
+  ngOnInit(){
+
+    this.employee.getUser()
+    .subscribe( data => console.log(data)  );
+  }
 }
---
+++ src/app/service/employee.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUser(){
+    return this.httpClient.get(`http://localhost:8080/employeepayrollservice/get`);
+  }
+}
+
